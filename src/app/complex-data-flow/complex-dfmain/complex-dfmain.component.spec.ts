2a6cf551053c4036413c340574bb87c85e84d401
Oleg <w.shevchuk.o@gmail.com>
2021-01-19 17:55:14 +0200

    Update to Angular v.11 done

--- src/app/complex-data-flow/complex-dfmain/complex-dfmain.component.spec.ts
+++ src/app/complex-data-flow/complex-dfmain/complex-dfmain.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ComplexDFMainComponent } from './complex-dfmain.component';
 
@@ -6,7 +6,7 @@ describe('ComplexDFMainComponent', () => {
   let component: ComplexDFMainComponent;
   let fixture: ComponentFixture<ComplexDFMainComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ComplexDFMainComponent ]
     })
